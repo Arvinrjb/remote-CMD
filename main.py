73346c2de4d39368928eb750b55a898818055b65
Arvin <97807007+Arvinrjb@users.noreply.github.com>
2024-02-16 17:03:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import socket
 from os import system
 import platform
+from colorama import Fore
 
 System = platform.system()
 
 
 def main():
-    host = input("Enter your ip: ")
+    host = input(Fore.BLUE + "Enter your ip: ")
     port = 8888
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(1)
     print(f"start listening on {host}:{port}")
-
     while True:
         client_socket, client_address = server_socket.accept()
+
         if System == "Linux":
             system('clear')
         elif System == "Windows":
             system('cls')
+
         print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
         command = client_socket.recv(1024).decode()
         system(command)
         print(command)
         client_socket.close()
+        print(f"start listening on {host}:{port}")
 
 
 main()
