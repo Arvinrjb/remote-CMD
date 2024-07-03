e3b7ffca1694a0576ea51a1c25a4125cc2090727
Arvin <arvinrjb13@gmail.com>
2024-07-03 22:42:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ def server_TCP(server_ip, server_port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((server_ip, port))
     server_socket.listen()
-    print(f"start listening on {server_ip}:{server_port}")
+    print(f"start listening TCP on {server_ip}:{server_port}")
     while True:
         client_socket, client_address = server_socket.accept()
-
         if System == "Linux":
             subprocess.run('clear', shell=True)
         elif System == "Windows":
@@ -26,18 +25,20 @@ def server_TCP(server_ip, server_port):
         print("Output:", result.stdout)
         if result.stderr:
             print("Error:", result.stderr)
-        print(Fore.BLUE + f"start listening on {server_ip}:{port}")
+        print(Fore.BLUE + f"start listening TCP on {server_ip}:{port}")
 
 def server_UDP(server_ip, server_port):
     server_address = (server_ip, server_port)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(server_address)
+    print(f"start listening UDP on {server_ip}:{server_port}")
     while True:
         command = server_socket.recvfrom(4096)
         result = subprocess.run(command[0].decode('utf-8'), shell=True, capture_output=True, text=True)
         print("Output:", result.stdout)
         if result.stderr:
             print("Error:", result.stderr)
+        print(f"start listening UDP on {server_ip}:{server_port}")
 while True:
     if server_protocol == "TCP":
         server_TCP(server_ip, port)
